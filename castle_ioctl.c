860fdc41ee9d871822f3bf462d47200034479e61
Grzegorz Miłoś <gregor@acunu.com>
2012-01-18 11:40:17 +0000

    trac-4160. Add an ability to query the state (inited/uninited) of the FS.

--- castle_ioctl.c
+++ castle_ioctl.c
@@ -101,6 +101,7 @@ int castle_merge_start(struct castle_front_connection *conn, c_merge_cfg_t merge
 #define C_PRINTF_work_size_t "%lu"
 #define C_PRINTF_pid "%u"
 #define C_PRINTF_c_da_opts_t "%lu"
+#define C_PRINTF_c_state_t "%d"
 
 #define CASTLE_IOCTL_0IN_0OUT(_id, _name)                                                         \
 int castle_##_id (struct castle_front_connection *conn)                                           \
